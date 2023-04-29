29e5501ca5309538c95d396823e6be09737c1184
LinWin-Cloud <2382546858@qq.com>
2023-04-29 12:27:37 +0800

    http_service

---
+++ http_service/src/HttpService/HttpFileContentType.java
@@ -0,0 +1,12 @@
+package HttpService;
+
+public class HttpFileContentType {
+    public String getType(String name) {
+        try {
+
+        }
+        catch (Exception exception) {
+            return "";
+        }
+    }
+}
