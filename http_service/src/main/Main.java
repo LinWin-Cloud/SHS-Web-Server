00ac244d17208414191e689ca2f0e4b0dbe04b36
LinWin-Cloud <2382546858@qq.com>
2023-05-02 21:18:53 +0800

    http_service

--- http_service/src/main/Main.java
+++ http_service/src/main/Main.java
@@ -8,7 +8,9 @@ import Web.Safe.DDOS;
 import main.JvmToolKit.Hotspot;
 
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -18,7 +20,7 @@ public class Main {
     public static int port = 0;
     public static String IP;
     public static String HtmlPath;
-    public static String ServerName = "LinwinSoft_SHS";
+    public static String ServerName = "LinwinSHS/";
     public static String ERROR_Page;
     public static SimpleJson Http_Service_Config = new SimpleJson();
     public static HttpService httpService = new HttpService();
@@ -34,6 +36,7 @@ public class Main {
     public static Hashtable<String,Integer> RequestsIP = new Hashtable<>();
     public static Hashtable<String,Integer> PHP_Requests = new Hashtable<>();
     public static int requests_php_number;
+    public static String version;
 
     public static void LoadConfig()
     {
@@ -76,6 +79,15 @@ public class Main {
         }
         Main.requests_php_number = Integer.parseInt(Http_Service_Config.get("php_requests"));
 
+        try {
+            FileReader fileReader = new FileReader("../config/version.md");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            Main.version = bufferedReader.readLine();
+            Main.ServerName = Main.ServerName + Main.version;
+        }
+        catch (Exception exception) {
+            System.exit(1);
+        }
     }
     public static void main(String[] args) {
         LoadConfig(); // load all the config file and project to the jvm
