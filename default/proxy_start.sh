4f67f0439b233868147ebbe67192c0f250b96cec
LinWin-Cloud <2382546858@qq.com>
2023-05-03 15:17:04 +0800

    default

--- default/proxy_start.sh
+++ default/proxy_start.sh
@@ -1,4 +1,4 @@
 basepath=$(cd `dirname $0`; pwd)
 cd $basepath/../bin/
-nohup ../shs/bin/shs-server -jar http_service.jar &
+nohup ../shs/bin/shs-proxy -jar proxy_service.jar &
 #../shs/bin/shs-server -jar http_service.jar
