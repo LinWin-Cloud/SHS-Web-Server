5d748d8b4c3c1aed9baf1d286f37568d314ab40e
LinWin-Cloud <2382546858@qq.com>
2023-05-03 14:41:04 +0800

    shs.py

---
+++ shs.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+if __name__ == "__main__":
+    if sys.avgc[1] == 'start':
+        print("1")
+
+    else:
+        print("""
+ 1- start               Start SHS.
+ 2- stop                Stop SHS.
+ 3- version             Show Version Information.
+        """)
+
+
