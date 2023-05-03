1df33a462eebae79976aeca809217670d58b8895
LinWin-Cloud <2382546858@qq.com>
2023-05-03 15:04:57 +0800

    shs.py

--- shs.py
+++ shs.py
@@ -2,25 +2,24 @@ import os
 import sys
 
 if __name__ == "__main__":
-    try:
+        if len(sys.argv) <= 1:
+            print(""" |-----LinwinSoft Shs Server-----|
+ 1- start               Start SHS.
+ 2- stop                Stop SHS.
+ |-------------------------------|""")
+            exit(0)
         if sys.argv[1] == 'start':
             os.system("/usr/shs-server/default/start.sh")
+            exit(0)
+
+        if sys.argv[1] == 'stop':
+            os.system('killall -9 shs-server')
+            exit(0)
 
         else:
-            print("""
- |-----LinwinSoft Shs Server-----|
+            print(""" |-----LinwinSoft Shs Server-----|
  1- start               Start SHS.
  2- stop                Stop SHS.
- 3- version             Show Version Information.
- |-------------------------------|
-            """)
-    except:
-        print("""
- |-----LinwinSoft Shs Server-----|
- 1- start               Start SHS.
- 2- stop                Stop SHS.
- 3- version             Show Version Information.
- |-------------------------------|
-            """)
+ |-------------------------------|""")
 
 
