f2fc0af73ebbe91bf7482303f68e77b4c6cd5ff1
LinWin-Cloud <2382546858@qq.com>
2023-05-03 15:17:23 +0800

    shell command

--- shs.py
+++ shs.py
@@ -9,17 +9,27 @@ if __name__ == "__main__":
  |-------------------------------|""")
             exit(0)
         if sys.argv[1] == 'start':
-            os.system("/usr/shs-server/default/start.sh")
+            os.system("sudo /usr/shs-server/default/start.sh")
             exit(0)
 
         if sys.argv[1] == 'stop':
-            os.system('killall -9 shs-server')
+            os.system('sudo killall -9 shs-server')
+            exit(0)
+
+        if sys.argv[1] == 'proxy_start':
+             os.system("sudo /usr/shs-server/default/proxy_start.sh")
+             exit(0)
+        
+        if sys.argv[1] == 'proxy_stop':
+            os.system("sudo killall -9 shs-proxy")
             exit(0)
 
         else:
             print(""" |-----LinwinSoft Shs Server-----|
  1- start               Start SHS.
  2- stop                Stop SHS.
+ 3. proxy_start         Start Proxy Server.
+ 4. proxy_stop          Stop Proxy Server.
  |-------------------------------|""")
 
 
