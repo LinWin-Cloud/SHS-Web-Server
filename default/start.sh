4f67b4c395ec083cfcbfbe601010012b01d394be
LinWin-Cloud <2382546858@qq.com>
2023-07-10 03:52:31 -0400

    default

--- default/start.sh
+++ default/start.sh
@@ -1,4 +1,4 @@
 basepath=$(cd `dirname $0`; pwd)
 cd $basepath/../bin/
-nohup ../shs/bin/shs-server -jar http_service.jar &
+nohup `cat /usr/shs-server/config/bootJDK.txt` -jar http_service.jar &
 #../shs/bin/shs-server -jar http_service.jar
